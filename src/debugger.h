7ad543629c82980bc668e2fcd1e753c20e7ca70b
Misha Farber Brodsky <misha@farberbrodsky.com>
2021-04-30 23:56:10 +0300

    Mostly planned out the header files for the beginning

---
+++ src/debugger.h
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/uio.h>  // process_vm_readv, process_vm_writev
+#include "event_queue.h"
+
+typedef struct {
+    pid_t pid;                   // debugged process pid
+    pthread_t waiter_thread;     // this thread is used for waitpid-ing in the background
+    pthread_mutex_t event_lock;  // mutex for using the event_queue
+    struct TinyDbg_EventQueue event_queue;
+} TinyDbg;
+
+// Start the debugger
+TinyDbg TinyDbg_start(const char *pathname, char *const argv, char *const envp);
+// Get the registers of the debugged process
+struct user_regs_struct TinyDbg_get_regsisters(TinyDbg *handle);
+// Set the registers of the debugged process
+void TinyDbg_set_regsisters(TinyDbg *handle, struct user_regs_struct regs);
+// Read the debugged process's memory
+void TinyDbg_read_memory(TinyDbg *handle, void *dst, void *src, size_t amount);
+// Write to the debugged process's memory
+void TinyDbg_write_memory(TinyDbg *handle, void *dst, void *src, size_t amount);
+// Set a breakpoint that is only hit once (automatically deleted)
+void TinyDbg_set_breakpoint_once(TinyDbg *handle, void *ip);
+// Continue a stopped process, using PTRACE_CONT
+void TinyDbg_continue(TinyDbg *handle);
+// Waits for an event, such as a breakpoint being hit or the process being stopped, and returns it.
+TinyDbg_Event TinyDbg_get_event();
+// Free a TinyDbg instance once it's done - delete the mutex, stop the thread, etc.
+void TinyDbg_free(TinyDbg *handle);
---
+++ src/event_queue.h
@@ -0,0 +1,16 @@
+enum TinyDbg_EventType {
+    TinyDbg_EVENT_BREAKPOINT,
+    TinyDbg_EVENT_STOP
+};
+
+struct TinyDbg_Event {
+    enum TinyDbg_EventType type;  // what event is this
+    void *data;                   // additional data, per event
+    struct TinyDbg_Event *next;   // the next event in the queue
+};
+void TinyDbg_event_free(struct TinyDbg_Event *event);  // frees an event
+
+struct TinyDbg_EventQueue {
+    struct TinyDbg_Event *head;
+    struct TinyDbg_Event *tail;
+};
