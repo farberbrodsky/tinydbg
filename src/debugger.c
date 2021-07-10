7e1a03e3db57f0e9016ac3aeeacbffb906143897
Misha Farber Brodsky <misha@farberbrodsky.com>
2021-07-10 15:36:54 +0300

    start work on v2

--- src/debugger.c
+++ src/debugger.c
@@ -1,221 +1,121 @@
 #include "debugger.h"
-#include <errno.h>
-
-static inline void event_append(TinyDbg *handle, struct TinyDbg_Event *new_event) {
-    pthread_mutex_lock(&handle->event_lock);
-    pthread_cond_broadcast(&handle->event_ready);
-    if (handle->event_queue.head == NULL) {
-        handle->event_queue.head = new_event;
-        handle->event_queue.tail = new_event;
-    } else {
-        handle->event_queue.tail->next = new_event;
-        handle->event_queue.tail = new_event;
-    }
-    pthread_mutex_unlock(&handle->event_lock);
-}
 
 static void waitpid_thread(TinyDbg *handle) {
     pid_t my_pid = handle->pid;
     while (true) {
-        pthread_mutex_lock(&handle->process_continued);  // wait for process to be continued
+        pthread_mutex_lock(&handle->process_continued);  // wait for process to be continued, which is when the lock is unlocked
         int wstatus;
         waitpid(my_pid, &wstatus, 0);
-        struct TinyDbg_Event *new_event = calloc(1, sizeof(struct TinyDbg_Event));
-        bool is_breakpoint = false;
-        if (wstatus == 2943) {
-            // should be a breakpoint, search for the info about it
-            struct user_regs_struct regs = TinyDbg_get_registers(handle);
-            void *ip = (void *)regs.rip;
-            pthread_mutex_lock(&handle->breakpoint_lock);
-            int i = 0;
-            for (; i < handle->breakpoints_len; i++) {
-                printf("Test against %p\n", ip);
-                if (handle->breakpoints[i].position == ip) {
-                    is_breakpoint = true;
-                    break;
-                }
-            }
-            if (is_breakpoint) {
-                new_event->type = TinyDbg_EVENT_BREAKPOINT;
-                void *breakpoint_clone = malloc(sizeof(TinyDbg_Breakpoint));
-                memcpy(breakpoint_clone, &handle->breakpoints[i], sizeof(TinyDbg_Breakpoint));
-                new_event->data = breakpoint_clone;
-            }
-            pthread_mutex_unlock(&handle->breakpoint_lock);
+        pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+        if (WIFEXITED(wstatus)) {
+            // debugged program is done
+            TinyDbg_Event *event = malloc(sizeof(TinyDbg_Event));
+            event->type = TinyDbg_event_type_exit;
+            event->content.stop_code = WEXITSTATUS(wstatus);
+            EventQueue_add(handle->eq_debugger_events, event);
         }
-        if (!is_breakpoint) {
-            new_event->type = TinyDbg_EVENT_STOP;
-            // create a pointer to wstatus, and put it in the event
-            int *wstatus_ptr = malloc(sizeof(int));
-            *wstatus_ptr = wstatus;
-            new_event->data = wstatus_ptr;
-        }
-        // add to the event queue
-        event_append(handle, new_event);
+        pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
     }
 }
 
-TinyDbg *TinyDbg_start(const char *filename, char *const argv[], char *const envp[]) {
-    int child_pid = vfork();  // like fork but blocking, and without cloning anything, for efficiency
+struct process_manager_thread_args {
+    TinyDbg *handle;
+    const char *filename;
+    char *const *argv;
+    char *const *envp;
+};
+
+static void process_manager_thread(struct process_manager_thread_args *args) {
+    TinyDbg *handle = args->handle;
+    const char *filename = args->filename;
+    char *const *argv = args->argv;
+    char *const *envp = args->envp;
+    free(args);
+
+    pid_t child_pid = vfork();
     if (child_pid == 0) {
         // am child
         ptrace(PTRACE_TRACEME, 0, 0, 0);
         execve(filename, argv, envp);
-        // execve does not return
     } else {
-        // am parent
-        // wait for process to be ready
+        pthread_mutex_lock(&handle->process_continued);  // process is currently stopped
+        handle->pid = child_pid;
         waitpid(child_pid, NULL, 0);
-        // create object
-        TinyDbg *result = malloc(sizeof(TinyDbg));
-        result->pid = child_pid;
-        // events
-        pthread_mutex_init(&result->event_lock, NULL);
-        pthread_cond_init(&result->event_ready, NULL);
-        pthread_mutex_init(&result->process_continued, NULL);
-        pthread_mutex_lock(&result->process_continued);
-        result->event_queue.head = NULL;
-        result->event_queue.tail = NULL;
-        // breakpoints
-        pthread_mutex_init(&result->breakpoint_lock, NULL);
-        result->breakpoints_len = 0;
-        result->breakpoint_positions = malloc(0);
-        result->breakpoints = malloc(0);
-        // create waiter thread
-        pthread_create(&result->waiter_thread, NULL, (void * (*)(void *))&waitpid_thread, result);
-        return result;
+        ptrace(PTRACE_SETOPTIONS, child_pid, NULL, PTRACE_O_EXITKILL);  // don't let the traced process run after i'm done
+
+        EventQueue_Consumer *consumer = EventQueue_new_consumer(handle->eq_process_manager);
+        void *data; EventQueue_consume(consumer, &data);  // i'm ready now, the creator should push an empty event
+
+        while (true) {
+            TinyDbg_procman_request *data;
+            if (EventQueue_consume(consumer, (void **)(&data)) == 'K') return EventQueue_destroy_consumer(consumer);
+            if (data->type == TinyDbg_procman_request_type_continue) {
+                // continue
+                ptrace(PTRACE_CONT, handle->pid, 0, 0);
+                pthread_mutex_unlock(&handle->process_continued);  // process is currently continued
+            }
+            if (data->type == TinyDbg_procman_request_type_stop) {
+                // send a sigstop
+                kill(handle->pid, SIGSTOP);
+            }
+
+            free(data);
+        }
     }
+}
+
+TinyDbg *TinyDbg_start(const char *filename, char *const argv[], char *const envp[]) {
+    // create the object
+    TinyDbg *result = calloc(1, sizeof(TinyDbg));
+
+    result->breakpoint_positions = malloc(0);
+    result->breakpoints = malloc(0);
+    result->breakpoints_len = 0;
+
+    pthread_mutex_init(&result->process_continued, NULL);
+    result->eq_process_manager = EventQueue_new();
+    result->eq_debugger_events = EventQueue_new();
+
+    struct process_manager_thread_args *procman_args = malloc(sizeof(struct process_manager_thread_args));
+    procman_args->handle = result;
+    procman_args->filename = filename;
+    procman_args->argv = argv;
+    procman_args->envp = envp;
+
+    pthread_create(&result->process_manager_thread, NULL, (void * (*)(void *))&process_manager_thread, procman_args);
+    // send empty join to process_manager_thread, it will return only once it's done ptracing
+    EventQueue_join(EventQueue_add_joinable(result->eq_process_manager, NULL));
+
+    // create the waiter thread, now that the pid is defined
+    pthread_create(&result->waiter_thread, NULL, (void * (*)(void *))&waitpid_thread, result);
+
+    return result;
 }
 
 void TinyDbg_free(TinyDbg *handle) {
-    // events
-    pthread_cancel(handle->waiter_thread);              // close waiter thread
-    pthread_mutex_unlock(&handle->process_continued);   // if waiting on the process to continue, don't
-    pthread_join(handle->waiter_thread, NULL);          // wait for it to close
-    pthread_mutex_destroy(&handle->event_lock);         // destroy the event mutex
-    pthread_cond_destroy(&handle->event_ready);
-    pthread_mutex_destroy(&handle->process_continued);  // destroy the process_continued mutex
-    // free all events
-    struct TinyDbg_Event *event = handle->event_queue.head;
-    while (event != NULL) {
-        struct TinyDbg_Event *next = event->next;
-        TinyDbg_Event_free(event);
-        event = next;
-    }
-    // breakpoints, should be ok to destroy because other threads are already down
-    pthread_mutex_destroy(&handle->breakpoint_lock);
-    handle->breakpoints_len = 0;
-    // NOTE right now breakpoints don't have any malloc'd pointers, but if they do we'd need to free them
     free(handle->breakpoint_positions);
+    // TODO free all breakpoints
     free(handle->breakpoints);
-    // finishing touches
+
+    pthread_mutex_destroy(&handle->process_continued);
+    EventQueue_free(handle->eq_process_manager);
+    EventQueue_free(handle->eq_debugger_events);
+
+    pthread_cancel(handle->waiter_thread);
+    pthread_cancel(handle->process_manager_thread);
     free(handle);
 }
 
-struct user_regs_struct TinyDbg_get_registers(TinyDbg *handle) {
-    struct user_regs_struct regs;
-    if (ptrace(PTRACE_GETREGS, handle->pid, 0, &regs) == -1) {
-        printf("PANIC!\n");
-        exit(1);
-    }
-    return regs;
+static EventQueue_JoinHandle *TinyDbg_send_empty_procman_request(TinyDbg *handle, TinyDbg_procman_request_type type) {
+    TinyDbg_procman_request *data = calloc(1, sizeof(TinyDbg_procman_request));
+    data->type = type;
+    return EventQueue_add_joinable(handle->eq_process_manager, data);
 }
 
-void TinyDbg_set_regsisters(TinyDbg *handle, struct user_regs_struct regs) {
-    ptrace(PTRACE_SETREGS, handle->pid, 0, &regs);
+// Wrappers for the previous function
+EventQueue_JoinHandle *TinyDbg_stop(TinyDbg *handle) {
+    return TinyDbg_send_empty_procman_request(handle, TinyDbg_procman_request_type_stop);
 }
-
-int TinyDbg_read_memory(TinyDbg *handle, void *dest, void *src, size_t amount) {
-    struct iovec iov_remote = {src, amount};
-    struct iovec iov_local = {dest, amount};
-    pthread_mutex_lock(&handle->breakpoint_lock);
-    int result = process_vm_readv(handle->pid, &iov_local, 1, &iov_remote, 1, 0);
-    pthread_mutex_unlock(&handle->breakpoint_lock);
-    if (result < 0) return result;
-    return 0;
-}
-
-/*
-int TinyDbg_write_memory(TinyDbg *handle, void *dest, void *src, size_t amount) {
-    struct iovec iov_local = {src, amount};
-    struct iovec iov_remote = {dest, amount};
-    // it currently results with EFAULT, which means it's out of the address space, idk why
-    return process_vm_writev(handle->pid, &iov_local, 1, &iov_remote, 1, 0);
-}
-changed to /proc/pid/mem because it seems to be working when process_vm_writev isn't
-*/
-int TinyDbg_write_memory(TinyDbg *handle, void *dest, void *src, size_t amount) {
-    char mem_file_str[31]; // len("/proc/18446744073709551616/mem0") is 31 (64-bit max)
-    sprintf(mem_file_str, "/proc/%d/mem", handle->pid);
-    pthread_mutex_lock(&handle->breakpoint_lock);
-    FILE *mem_file = fopen(mem_file_str, "w");
-    fseek(mem_file, (long)dest, SEEK_SET);
-    int result = fwrite(src, amount, 1, mem_file);
-    if (result < 0) { fclose(mem_file); pthread_mutex_unlock(&handle->breakpoint_lock); return result; }
-    fclose(mem_file);
-    pthread_mutex_unlock(&handle->breakpoint_lock);
-    return 0;
-}
-
-int TinyDbg_set_breakpoint_once(TinyDbg *handle, void *ip) {
-    char original;
-    char int3 = '\x03';
-    if (TinyDbg_read_memory(handle, &original, ip, 1) != 0) return -1;  // read original content
-    if (TinyDbg_write_memory(handle, ip, &int3, 1) != 0) return -2;     // write the int3
-    pthread_mutex_lock(&handle->breakpoint_lock);
-    handle->breakpoints_len += 1;
-    handle->breakpoints = realloc(handle->breakpoints, handle->breakpoints_len * sizeof(TinyDbg_Breakpoint));
-    handle->breakpoints[handle->breakpoints_len - 1].is_once = true;
-    handle->breakpoints[handle->breakpoints_len - 1].original = original;
-    handle->breakpoints[handle->breakpoints_len - 1].position = ip;
-    // TODO write this to a list of breakpoints, so we can know whether it's for one time or not
-    pthread_mutex_unlock(&handle->breakpoint_lock);
-    return 0;
-}
-
-void TinyDbg_Breakpoint_print(TinyDbg_Breakpoint *breakpoint) {
-    printf("Breakpoint(is_once = %d, position = %p)\n", breakpoint->is_once, breakpoint->position);
-}
-
-void TinyDbg_continue(TinyDbg *handle) {
-    // lock process_continued again, worst case scenario you get EDEADLK for already owning it
-    pthread_mutex_trylock(&handle->process_continued);
-    pthread_mutex_unlock(&handle->process_continued);  // unlock it for real now
-    ptrace(PTRACE_CONT, handle->pid, 0, 0);
-}
-
-static struct TinyDbg_Event *TinyDbg_get_event_nowait_nolock(TinyDbg *handle) {
-    // pop the first event
-    struct TinyDbg_Event *current_head = handle->event_queue.head;
-    if (current_head == NULL) {
-        pthread_mutex_unlock(&handle->event_lock);
-        return NULL;
-    }
-
-    handle->event_queue.head = current_head->next;
-
-    if (current_head->next == NULL) {
-        // the queue is empty now
-        handle->event_queue.tail = NULL;
-    }
-
-    return current_head;
-}
-
-struct TinyDbg_Event *TinyDbg_get_event_nowait(TinyDbg *handle) {
-    pthread_mutex_lock(&handle->event_lock);
-    struct TinyDbg_Event *result = TinyDbg_get_event_nowait_nolock(handle);
-    pthread_mutex_unlock(&handle->event_lock);
-    return result;
-}
-
-struct TinyDbg_Event *TinyDbg_get_event(TinyDbg *handle) {
-    struct TinyDbg_Event *current_result = TinyDbg_get_event_nowait(handle);
-    if (current_result != NULL) return current_result;
-    // have to wait...
-    pthread_mutex_lock(&handle->event_lock);
-    pthread_cond_wait(&handle->event_ready, &handle->event_lock);
-    struct TinyDbg_Event *result = TinyDbg_get_event_nowait_nolock(handle);
-    pthread_mutex_unlock(&handle->event_lock);
-    return result;
+EventQueue_JoinHandle *TinyDbg_continue(TinyDbg *handle) {
+    return TinyDbg_send_empty_procman_request(handle, TinyDbg_procman_request_type_continue);
 }
--- src/debugger.h
+++ src/debugger.h
@@ -1,18 +1,18 @@
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
 #endif
-#ifndef GUARD_24e5ae45_8058_4b87_9cc3_e06d5681082b
-#define GUARD_24e5ae45_8058_4b87_9cc3_e06d5681082b
-#include <stdint.h>
+#ifndef GUARD_DEBUGGER_H
+#define GUARD_DEBUGGER_H
+#include <errno.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <stdint.h>
 #include <string.h>
-#include <stdbool.h>
-#include <sys/uio.h>   // process_vm_readv, process_vm_writev
 #include <pthread.h>
+#include <sys/uio.h>
 #include <sys/wait.h>
-#include <sys/user.h>  // user_regs_struct
 #include <sys/ptrace.h>
-#include "event_queue.h"
+#include "../event_queue_c/event_queue.h"
 
 // Breakpoints are stored in pointers.
 // They are freed with free() because they don't store any more pointers
@@ -24,39 +24,65 @@ typedef struct {
 
 typedef struct {
     pid_t pid;                          // debugged process pid
+
     uintptr_t *breakpoint_positions;    // array of breakpoint positions
     TinyDbg_Breakpoint *breakpoints;    // array of breakpoint data, with the same indexes, this is for locality (cpu caching)
     size_t breakpoints_len;             // how many breakpoints are there
-    pthread_mutex_t breakpoint_lock;    // mutex for reading/setting breakpoints, OR READING/WRITING TO PROCESS MEMORY
+
     pthread_t waiter_thread;            // this thread is used for waitpid-ing in the background
-    pthread_mutex_t event_lock;         // mutex for using the event_queue
-    pthread_cond_t event_ready;         // broadcast to when there is an event ready to be added
-    pthread_mutex_t process_continued;  // unlocked when the process is continued
-    struct TinyDbg_EventQueue event_queue;
+    pthread_t process_manager_thread;   // this thread manages the process - ptraces and reads/writes to memory
+
+    pthread_mutex_t process_continued;  // locked/unlocked by manager when the process is continued
+
+    EventQueue *eq_process_manager;     // event queue for the process manager - send your ptrace/memory/breakpoint requests here
+    EventQueue *eq_debugger_events;     // event queue for events e.g. breakpoint hit or process stopped
 } TinyDbg;
 
 // Start the debugger
 TinyDbg *TinyDbg_start(const char *filename, char *const argv[], char *const envp[]);
+
 // Free a TinyDbg instance once it's done - delete the mutex, stop the thread, etc.
 void TinyDbg_free(TinyDbg *handle);
-// Get the registers of the debugged process
-struct user_regs_struct TinyDbg_get_registers(TinyDbg *handle);
-// Set the registers of the debugged process
-void TinyDbg_set_regsisters(TinyDbg *handle, struct user_regs_struct regs);
-// Read the debugged process's memory. Returns 0 on success, otherwise -1 and errno is set by process_vm_readv.
-int TinyDbg_read_memory(TinyDbg *handle, void *dest, void *src, size_t amount);
-// Write to the debugged process's memory. Returns 0 on success, otherwise -1 and errno is set by process_vm_writev.
-int TinyDbg_write_memory(TinyDbg *handle, void *dest, void *src, size_t amount);
-// Set a breakpoint that is only hit once (automatically deleted), returns -1 if couldn't read or -2 if couldn't write.
-// errno is set like in TinyDbg_read_memory and in TinyDbg_write_memory.
-int TinyDbg_set_breakpoint_once(TinyDbg *handle, void *ip);
-// Prints a breakpoint to stdout for, well, debugging
-void TinyDbg_Breakpoint_print(TinyDbg_Breakpoint *breakpoint);
-// Continue a stopped process, using PTRACE_CONT
-void TinyDbg_continue(TinyDbg *handle);
-// Get the first event, such as a breakpoint being hit or the process being stopped, and return it (or NULL).
-struct TinyDbg_Event *TinyDbg_get_event_nowait(TinyDbg *handle);
-// Wait for an event, such as a breakpoint being hit or the process being stopped, and return it.
-struct TinyDbg_Event *TinyDbg_get_event(TinyDbg *handle);
+
+// TODO define the event structs and the union and enum for them
+typedef enum {
+    TinyDbg_procman_request_type_stop,        // no content
+    TinyDbg_procman_request_type_continue,    // no content
+    TinyDbg_procman_request_type_get_regs,    // unimplemented, content should be a pointer to struct user_regs_struct
+    TinyDbg_procman_request_type_set_regs,    // unimplemented, content should be a pointer to struct user_regs_struct
+    TinyDbg_procman_request_type_get_mem,     // unimplemented
+    TinyDbg_procman_request_type_set_mem,     // unimplemented
+    TinyDbg_procman_request_type_get_breakp,  // unimplemented
+    TinyDbg_procman_request_type_set_breakp,  // unimplemented
+} TinyDbg_procman_request_type;
+
+typedef struct {
+    struct iovec local_iov;
+    struct iovec remote_iov;
+} TinyDbg_procman_request_get_mem;
+
+typedef struct {
+    struct iovec local_iov;
+    struct iovec remote_iov;
+} TinyDbg_procman_request_set_mem;
+
+typedef struct {
+    TinyDbg_procman_request_type type;
+    void *content;
+} TinyDbg_procman_request;
+
+typedef enum {
+    TinyDbg_event_type_exit,
+} TinyDbg_Event_type;
+
+typedef struct {
+    TinyDbg_Event_type type;
+    union TinyDbg_Event_content {
+        int stop_code;
+    } content;
+} TinyDbg_Event;
+
+EventQueue_JoinHandle *TinyDbg_stop(TinyDbg *handle);
+EventQueue_JoinHandle *TinyDbg_continue(TinyDbg *handle);
 
 #endif
